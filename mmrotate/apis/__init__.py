a4a465015c3a112642788eec934b4b3d097e264f
erenzhou <592267829@qq.com>
2022-04-14 22:32:10 +0800

    add init_detector

--- mmrotate/apis/__init__.py
+++ mmrotate/apis/__init__.py
@@ -1,5 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .inference import inference_detector_by_patches
+from .inference import inference_detector_by_patches, init_detector
 from .train import train_detector
 
-__all__ = ['inference_detector_by_patches', 'train_detector']
+__all__ = ['inference_detector_by_patches', 'train_detector', 'init_detector']
